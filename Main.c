a2891928d7e9a65bb82386b02beef608cbc58882
rayssadavvila <rayssamunizd@gmail.com>
2023-09-18 13:07:44 +0000

    Iniciando a implementação com a typedef struct

---
+++ Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char nome_do_livro[20];
+    int codigo_do_livro;
+    char categoria_do_livro[20];
+} ficha_catalografica;
+
+int main(){
+
+    return 0;
+}
